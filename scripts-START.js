0689baddb1461fa83531155b168dc02cce14a579
Caleb Lee <caleb.gwl@gmail.com>
2017-08-11 14:01:10 -0500

    Be back at function

--- scripts-START.js
+++ scripts-START.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -21,6 +23,15 @@ function displayTimeLeft(seconds) {
   const remainderSeconds = seconds % 60;
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${
     remainderSeconds}`;
-    document.title = display;
+  document.title = display;
   timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  endTime.textContent = `Be Back at ${adjustedHour}:${minutes < 10 ? 0 : ''}${minutes}`;
 }
